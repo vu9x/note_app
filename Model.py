0dbc2b62e3e6e69276bdc2e3281570c005459691
Vu <nguyentuanvu9x@gmail.com>
2023-06-25 02:36:03 +0300

    Finished: class Model.py

--- Model.py
+++ Model.py
@@ -1,13 +1,16 @@
 import os
 import csv
-import datetime
+from datetime import datetime
 
 class Editor_csv:
-
     def __init__(self, file_name):
-        self.__path = os.path.join(os.getcwd(), "working_folder")
         self.__file_name = file_name + '.csv'
+        self.__path = os.path.join(os.getcwd(), "working_folder")
         self.__file_path = os.path.join(self.get_path, self.get_file_name)
+
+        #Create Working Directory if not exist
+        if not os.path.exists(fr'{self.get_path}'):
+            os.mkdir(fr'{self.get_path}')
 
     @property
     def get_path(self):
@@ -21,61 +24,34 @@ class Editor_csv:
     def get_file_path(self):
         return self.__file_path
 
-    # @get_file_name.setter
-    # def set_file_name(self, file_name: str):
-    #     self.__file_name = file_name + '.csv'
-    #
-    # @get_file_path.setter
-    # def set_file_path(self):
-    #     self.__file_path = os.path.join(self.get_path, self.get_file_name)
-    #
-    # def abs_path(self, file_name):
-    #     self.set_file_name = file_name
-    #     self.set_file_path
-
     def create_file(self):
-        print(self.get_path)
-        print(self.get_file_name)
-        print(self.get_file_path)
-        if not os.path.exists(fr'{self.get_path}'):
-            os.mkdir(fr'{self.get_path}')
-
         with open(fr'{self.get_file_path}', 'a+') as file:
             print(self.get_file_name, " создан")
 
-
-    def read_file(cls, file_path):
+    def read_file(self):
         # with open(cls.get_file_path, 'r') as file:
-        with open(fr'{file_path}', 'r') as file:
+        with open(fr'{self.get_file_path}', 'r') as file:
             reader = csv.DictReader(file)
             notes = list(reader)
 
-        filled_note_file = Note
         for note in notes:
-            Note(
-                id=note.get('id'),
-                header=note.get('header'),
-                body=note.get('body'),
-                created_at=note.get('created_at'),
-                modified_at=note.get('modified_at')
-            )
-        return filled_note_file
+            tmp = Note(header=note.get('header'),
+                       body=note.get('body'),
+                       id=note.get('id'),
+                       created_at=note.get('created_at'),
+                       modified_at=note.get('modified_at'))
 
-    @classmethod
-    def save_file(cls):
-        with open(cls.get_file_path, 'w') as file:
+        return Note.note_list
+
+    def save_file(self, Note_List):
+        with open(self.get_file_path, 'w') as file:
             writer = csv.DictWriter(file, fieldnames=['id', 'header', 'body', 'created_at', 'modified_at'])
             writer.writeheader()
-            writer.writerows(Note.Note_list)
+            writer.writerows(Note_List)
 
-    @classmethod
     def delete_file(cls):
         os.remove(cls.get_file_path)
 
-    @classmethod
-    def listf_in_directory(cls):
-        files = [f for f in os.listdir(cls.get_path) if os.path.isfile(f)]
-        return files
 
 class Note:
     note_list = []
@@ -84,96 +60,77 @@ class Note:
     def __init__(self, header: str, body: str, id=None, created_at=None, modified_at=None):
 
         #Attributes should be set without user prompt
-        if id is None:
-            self.__id = self.set_id()
-        else:
-            self.__id = id
-
-        if created_at is None:
-            self.set_created()
-        else:
-            self.__created_at = created_at
-
-        if modified_at is None:
-            self.set_modified()
-        else:
-            self.__modified_at = modified_at
+        self.__id = str(len(Note.note_list) + 1) if id is None else id
+        self.__created_at = datetime.now() if created_at is None else created_at
+        self.__modified_at = datetime.now() if modified_at is None else modified_at
 
         #Attributes set by user prompt
         self.__header = header
         self.__body = body
-
         # Add the note to the class list
-        Note.note_list.append({
-            "id": self.get_id(),
-            "created_at": self.get_created(),
-            "modified_at": self.get_modified(),
-            "header": self.get_header(),
-            "body": self.get_body()
-        })
+        Note.note_list.append({'id':self.__id,
+                               'header':self.__header,
+                               'body':self.__body,
+                               'created_at': self.__created_at,
+                               'modified_at': self.__modified_at})
 
+    # Note Getters
     @property
-    def get_id(self):
+    def id(self):
         return self.__id
 
-    @get_id.setter
-    def set_id(self):
-        self.__id = len(Note.note_list)
-
     @property
-    def get_created(self):
+    def created(self):
         return self.__created_at
 
-    @get_created.setter
-    def set_created(self):
-        self.__created_at = datetime.now()
-
     @property
-    def get_modified(self):
+    def modified(self):
         return self.__modified_at
 
-    @get_modified.setter
-    def set_modified(self):
-        self.__modified_at = datetime.now()
-
     @property
-    def get_header(self):
+    def header(self):
         return self.__header
 
-    @get_header.setter
-    def set_header(self, header: str):
-        self.__header = header
-
     @property
-    def get_body(self):
+    def body(self):
         return self.__body
 
-    @get_body.setter
-    def set_body(self, body: str):
+    # Note setters
+    @id.setter
+    def set_id(self, value):
+        self.__id = value
+
+    @created.setter
+    def set_created(self, value):
+        self.__created_at = value
+
+    @modified.setter
+    def set_modified(self, value):
+        self.__modified_at = value
+
+    @header.setter
+    def header(self, header: str):
+        self.__header = header
+
+    @body.setter
+    def body(self, body: str):
         self.__body = body
 
     def __str__(self):
-        message = f"create: {self.get_created()}\n" \
-                  f"las modify: {self.get_modified()}\n" \
-                  f"id: {self.get_id()}\n" \
-                  f"header: {self.get_header()}\n" \
-                  f"body: {self.get_body()}"
-
-        print(message, "_" * 10)
+        message = f"id: {self.id}, " \
+                  f"create: {self.created}, " \
+                  f"last modify: {self.modified}, " \
+                  f"header: {self.header}, " \
+                  f"body: {self.body}"
+        return message
 
     def __repr__(self):
-        return f"id: {self.get_id()}, {self.get_header()}"
+        return str({'id':self.id,
+                'header':self.header,
+                'body':self.body,
+                'created_at':self.created,
+                'modified_at':self.modified})
 
-    # @classmethod
-    # def edit_note(cls, index: int, header: str, body:str):
-    #     Note.note_list[index - 1]['header'] = header
-    #     Note.note_list[index - 1]['body'] = body
-    #     Note.note_list[index - 1]['modified_at'] = modified()
-
-    def remove_note(self, index=int):
+    @classmethod
+    def remove_note(cls, index):
         return Note.note_list.pop(index-1)
-
-# c = Editor_csv("test")
-# print(type(c.get_file_name))
-# print(type(c.get_path))
-# print(c.get_file_path)
