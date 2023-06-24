7ca07965bafdab4e1b35fad2d1d40441b8154aed
Vu <nguyentuanvu9x@gmail.com>
2023-06-25 02:35:38 +0300

    Finished: class Controller

--- Controller.py
+++ Controller.py
@@ -1,9 +1,9 @@
 import Model
-from View import View
 import os
+from View import View
+from datetime import datetime
 
 class Controller:
-
     def __init__(self, Model, View):
         self.__model = Model
         self.__view = View
@@ -15,40 +15,88 @@ class Controller:
     def get_view(self):
         return self.__view
 
-    def show_file_menu(self):
-        self.get_view.show_file_menu()
+    def hello_world(self):
+        self.get_view.hello_world()
 
-    def show_note_menu(self):
-        self.get_view.show_note_menu()
+    # Show files in working directory
+    def files_in_directory(self):
+        files = os.listdir(os.path.join(os.getcwd(), "working_folder"))
+        file_names = []
+        for file in files:
+            file_names.append(file.split('.')[0])
+        return file_names
+    def show_files_in_directory(self, file_names):
+        self.get_view.show_files_in_directory(file_names)
 
     def file_manager(self):
-        choices = ['1', '2', '3', '4', '5']
-        self.show_file_menu()
-        choice = self.get_view.user_choice()
+        #User input
+        file_names = self.files_in_directory()
+        self.get_view.show_file_menu()
+        editor_commands = ['1', '2', '3']
 
-        if choice in choices:
-            name = input("Введите название файла: ")
-            editor = self.get_model.Editor_csv(name)
-        else:
-            print("Вы ввели не существующую команду")
+        command = self.get_view.show_command_input()
 
-        match choice:
-            case '1':
-                return editor.create_file()
-            case '2':
-                my_notes = self.get_model.Editor_csv.read_file(self.get_model.Editor_csv.get_file_path)
-                self.get_view.print_my_notes(my_notes)
-                note_manager()
-                return
+        if command in editor_commands:
+            file_name = self.get_view.show_file_name()
+            editor = self.get_model.Editor_csv(file_name)
 
-    def note_manager(self, notes):
-        self.get_view.show_note_menu()
+        if command == '1':
+            editor.create_file()
+        elif command == '2':
+            if file_name in file_names:
+                editor.read_file()
+                self.note_manager(editor)
+            else: print("Файл не существует")
+        elif command == '3':
+            editor.delete_file() if file_name in file_names else print("Файл не существует")
+        elif command == '4':
+            file_names = self.files_in_directory()
+            self.show_files_in_directory(file_names)
+        elif command == '5':
+            return False
+        return True
 
+    def note_manager(self, editor):
+        while True:
+            note_list = self.get_model.Note.note_list
+            commands = ['1', '2', '3', '4', '5']
+            self.get_view.show_note_menu()
+            command = self.get_view.show_command_input()
+
+            if command == '1':
+                header = self.get_view.show_header_input()
+                body = self.get_view.show_body_input()
+                my_note = self.get_model.Note(header=header, body=body)
+            elif command == '2':
+                self.get_view.show_my_notes(note_list)
+            elif command == '3':
+                index = self.get_view.show_note_index()
+                header = self.get_view.show_header_input()
+                body = self.get_view.show_body_input()
+
+                for note in note_list:
+                    if note.get('id') == index:
+                        note['header'] = header
+                        note['body'] = body
+                        note['modified_at'] = datetime.now()
+
+            elif command == '4':
+                editor.save_file(note_list)
+            elif command == '5':
+                index = self.get_view.show_note_index()
+                for note in note_list:
+                    if note.get('id') == index:
+                        self.get_model.Note.note_list.remove(note)
+            elif command == '6':
+                return False
 
 
 ctr = Controller(Model, View)
-ctr.file_manager()
 
-# v = os.path.join(os.getcwd(), "working_folder")
-# print(type(v))
-# print(v)
+ctr.hello_world()
+ctr.files_in_directory()
+command = True
+while command is True:
+    command = ctr.file_manager()
+
+print("Вы вышли из программы")
